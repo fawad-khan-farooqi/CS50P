54ef334d5e1a31c618ac2b9ff767f4e2d16f2da7
Fawad Khan Farooqi <fawadkhanfarooqi@gmail.com>
2026-06-23 00:16:36 +0500

    multiplication_table_reverse

--- Practice/multiplication_table_reverse.py
+++ Practice/multiplication_table_reverse.py
@@ -1,8 +1,9 @@
+# Multiplication table in reverse
 while True:
     try:
         num = int(input("Enter a num? "))
         break
     except ValueError:
         print("Invalid Input")
-for i in range(0, 10):
-    print(f"{num} x {i+1} = {5*(i+1)}")
+for i in range(10, 0, -1):
+    print(f"{num} x {i} = {num*i}")
