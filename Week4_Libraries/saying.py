7c4308e601a5ec96b1a02783b2622d1e87ac8259
Fawad Khan Farooqi <fawadkhanfarooqi@gmail.com>
2026-06-26 10:13:42 +0500

    Add Week 4 libraries lecture notes

--- Week4_Libraries/saying.py
+++ Week4_Libraries/saying.py
@@ -1,10 +1,11 @@
 import sys
-from mymodule import hello
-from mymodule import goodbye
+# from mymodule import hello
+# from mymodule import goodbye
+import mymodule
 
 if len(sys.argv) < 2:
     sys.exit()
 
-hello(sys.argv[1])
-goodbye(sys.argv[1])
+mymodule.hello(sys.argv[1])
+mymodule.goodbye(sys.argv[1])
 
